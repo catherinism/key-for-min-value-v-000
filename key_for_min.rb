8943a1401898cb75afb64beacfe75a89a926f6b9
Catherine Salazar <catcreates13@gmail.com>
2018-01-27 03:41:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   name_hash.each {|key, value|}
   if min_value == nil || value < min_value
-    min_key = min_value
+    min_value = min_key
   end
   min_key
 
